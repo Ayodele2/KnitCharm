e7661ea4492deb8622a60a100506e944e625798f
Kabiyesi <ayodeleson28@gmail.com>
2026-01-26 03:47:42 +0100

    admin routes added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useUser
+} from "@clerk/clerk-react";
+
 
 
 const App = () => {
+ 
+
   return (
     <div>
-      <h1>
-        Home Page 
-      </h1>
+      <h1>welcome to Knit efcharm</h1>
 
-      <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+       <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+
+      
     </div>
   )
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,46 @@
-import express from 'express';
-import path from 'path';
-import { ENV } from './config/env.js';
-import { connectDB } from './config/db.js';
+import express from "express";
+import path from "path";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import cors from "cors";
+
+import { functions, inngest } from "./config/inngest.js";
+
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+import adminRoutes from "./routes/admin.route.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-app.use(clerkMiddleware()) // adds auth object under the req => req.auth 
+app.use(express.json());
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
-app.get('/api/login', (req, res) => {
-    res.send('Hello World!');
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/admin", adminRoutes);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Success" });
 });
 
-// make app ready for deployment 
-if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../admin/dist")));
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-    app.get(/.*/, (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-    });
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-    connectDB();
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
+
+startServer();
